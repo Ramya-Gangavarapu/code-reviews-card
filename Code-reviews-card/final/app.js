192f3a860dd78ff6f407096100b170f459d33062
Ramya Gangavarapu <141222323+Ramya-Gangavarapu@users.noreply.github.com>
2023-11-01 12:33:20 +0530

    Add files via upload

---
+++ Code-reviews-card/final/app.js
@@ -0,0 +1,85 @@
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: 'susan smith',
+      job: 'web developer',
+      img: 'https://www.course-api.com/images/people/person-1.jpeg',
+      text: "I'm a web developer with a passion for coding and building awesome websites. In my free time, you can find me exploring new technologies, attending hackathons, and contributing to open-source projects. Let's create amazing things together!",
+    },
+    {
+      id: 2,
+      name: 'anna johnson',
+      job: 'web designer',
+      img: 'https://www.course-api.com/images/people/person-2.jpeg',
+      text: "As a web designer, I'm dedicated to creating beautiful and user-friendly designs that stand out. My design philosophy is inspired by simplicity and elegance. I enjoy bringing creative ideas to life through web design. Let's collaborate on your next project!",
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      img: 'https://www.course-api.com/images/people/person-4.jpeg',
+      text: "I'm an intern with a passion for learning and exploring new technologies. I'm eager to gain practical experience in the tech industry and contribute to exciting projects. My enthusiasm and dedication drive me to constantly improve and expand my skills.",
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      img: 'https://www.course-api.com/images/people/person-3.jpeg',
+      text: "I'm the boss of the team and love leading my talented colleagues. My experience and expertise in the tech industry have allowed me to build successful projects and businesses. I believe in a collaborative and creative work environment that fosters innovation and growth.",
+    },
+];
+
+// select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+});
+
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+// show next person
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+// show prev person
+prevBtn.addEventListener('click', function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+// show random person
+randomBtn.addEventListener('click', function () {
+  console.log('hello');
+
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
+});
